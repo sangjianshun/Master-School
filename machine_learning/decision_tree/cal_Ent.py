a2bd1beb8017cd197f18c25e62ce4f31efde0615
sangjianshun <sangjianshun@gmail.com>
2021-06-14 21:56:52 +0800

    test

--- machine_learning/decision_tree/cal_Ent.py
+++ machine_learning/decision_tree/cal_Ent.py
@@ -24,17 +24,29 @@ def cal_Gain(EntD,EntDv):
     assert float(total) == 1
     return Gain
 
+def cal_Gain_Ratio(Gain, Ent_a):
+    return Gain/Ent_a
+
+
 
 
 if __name__ == '__main__':
-    print(cal_Ent([3/8, 5/8]))
-    print(cal_Ent([1/2, 1/2, 0]))
-    print(cal_Ent([2/5, 3/5]))
-    print(cal_Ent([2/3,1/3]))
-    print(cal_Ent([1/5, 4/5]))
+    # print(cal_Ent([3/8, 5/8]))
+    # print(cal_Ent([1/2, 1/2, 0]))
+    # print(cal_Ent([2/5, 3/5]))
+    # print(cal_Ent([2/3,1/3]))
+    # print(cal_Ent([1/5, 4/5]))
+    # print(cal_Ent([3/8, 3/8, 2/8]))
+    # print(cal_Ent([2/5, 2/5, 1/5]))
 
-    print(cal_Gain(0.95, [[3/8,0],[5/8,0.97]]))
-    print(cal_Gain(0.95, [[3/8,0.92],[5/8,0.72]]))
-    print(cal_Gain(0.95, [[3/8,0.92],[3/8,0],[2/8,1]]))
-
-    print(cal_Gain(0.92, [[2/3,1],[1/3,0]]))
+    # print(cal_Gain(0.95, [[3/8,0],[5/8,0.97]]))
+    # print(cal_Gain(0.95, [[3/8,0.92],[5/8,0.72]]))
+    # print(cal_Gain(0.95, [[3/8,0.92],[3/8,0],[2/8,1]]))
+    print(cal_Gain(0.97, [[3/5,0.92],[2/5,0]]))
+    #
+    # print(cal_Gain(0.92, [[2/3,1],[1/3,0]]))
+    print(cal_Gain_Ratio(0.34,0.95))
+    print(cal_Gain_Ratio(0.16,0.95))
+    print(cal_Gain_Ratio(0.36,1.56))
+    print(cal_Gain_Ratio(0.97,1.52))
+    print(cal_Gain_Ratio(0.42,0.97))
