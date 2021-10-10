fda028578ca61971dd70d8a8303cd98cccfbed71
sangjianshun <sangjianshun@gmail.com>
2021-10-11 00:20:45 +0800

    test

--- python_high_level/metaclass_example.py
+++ python_high_level/metaclass_example.py
@@ -1,16 +1,16 @@
-class MetaClass(type):
-    def __init__(self, name, bases, dic):
-        super().__init__(name, bases, dic)
-        print("MetaClass.__init__")
-    def __new__(cls, *args, **kwargs):
-        print('MetaClass.__new__')
-        return type.__new__(cls, *args, **kwargs)
-    def __call__(cls, *args, **kwargs):
-        print('MetaClass.__call__')
-        obj = cls.__new__(cls)
-        cls.__init__(cls, *args, **kwargs)
-        return obj
-#
+# class MetaClass(type):
+#     def __init__(self, name, bases, dic):
+#         super().__init__(name, bases, dic)
+#         print("MetaClass.__init__")
+#     def __new__(cls, *args, **kwargs):
+#         print('MetaClass.__new__')
+#         return type.__new__(cls, *args, **kwargs)
+#     def __call__(cls, *args, **kwargs):
+#         print('MetaClass.__call__')
+#         obj = cls.__new__(cls)
+#         cls.__init__(cls, *args, **kwargs)
+#         return obj
+# #
 # class MyClass(metaclass=MetaClass):
 #     def __init__(self, name):
 #         print('MyClass.__init__')
@@ -19,22 +19,47 @@ class MetaClass(type):
 #     def __new__(cls, *args, **kwargs):
 #         print('MyClass.__new__')
 #         return object.__new__(cls)
-#
+
 # print("debug")
 # myClass = MyClass("myClass")
+# print("debug2")
+# myClass = MyClass.__call__("myClass")
 # print(1)
 
 
 
 
-
+# # 示例一：研究自定义类的底层原理
 # class MyClass():
 #     num = 100
 # myClass = MyClass()
 # print(type(myClass)) # <class '__main__.MyClass'>
 # print(type(MyClass)) # <class 'type'>
+# print(myClass) # <__main__.MyClass object at 0x000001F66FD1B220>
+# print(MyClass) # <class '__main__.MyClass'>
 #
-# print(myClass, MyClass) # <__main__.MyClass object at 0x0000022369DADDC0> <class '__main__.MyClass'>
+# MyClass2 = type("MyClass2", (), {"num":100})
+# myClass2 = MyClass2()
+# print(type(myClass2)) # <class '__main__.MyClass2'>
+# print(type(MyClass2)) # <class 'type'>
+# print(myClass2) # <__main__.MyClass2 object at 0x000001F66FD4E550>
+# print(MyClass2) # <class '__main__.MyClass2'>
+
+# 示例二，type的底层原理
+# MyClass2 = type("MyClass2", (), {"num":100})
+
+# MyClass2 = type.__call__("MyClass2", (), {"num":100})
+
+MyClass2 = type.__call__(type("MyClass2", (), {"num":100}))
+# type.__new__("MyClass2", (), {"num":100})
+# print(type.__init__("MyClass2", (), {"num":100}))
+myClass2 = MyClass2()
+print(type(myClass2)) # <class '__main__.MyClass2'>
+print(type(MyClass2)) # <class 'type'>
+print(myClass2) # <__main__.MyClass2 object at 0x000001F66FD4E550>
+print(MyClass2) # <class '__main__.MyClass2'>
+
+#
 #
 #
 # # class = type(classname, superclasses, attributedict)
@@ -43,9 +68,9 @@ class MetaClass(type):
 # # type.__init__(class, classname, superclasses, attributedict)
 #
 
-MyClass2 = MetaClass("MyClass2", (), {"num":100}) #
-print("debug")
-myClass2 = MyClass2()
-print(myClass2, MyClass2)
-print(myClass2.num)
+# MyClass2 = type("MyClass2", (), {"num":100}) #
+# print("debug")
+# myClass2 = MyClass2()
+# print(myClass2, MyClass2)
+# print(myClass2.num)
 
